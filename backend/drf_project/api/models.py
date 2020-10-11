bb27dc027c7ace2fbc8a5e4b1bf544ed703d9711
anavianm <anavian.max@gmail.com>
2020-10-11 15:34:56 -0400

    Posts model edit 1

--- backend/drf_project/api/models.py
+++ backend/drf_project/api/models.py
@@ -4,3 +4,19 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+
+
+class Posts(models.Model):
+    title = models.CharField(max_length=5000)
+    date = models.DateTimeField(auto_now_add = True)
+
+    # which category is better 
+    category = models.ManyToManyField(Category)
+    # category = models.ForeignKey(Category) 
+    tag = models.ManyToManyField(Categoy)
+    content = models.TextField()
+    img = models.ImageField(upload_to = user_directory_path) 
+    language = models.CharField(max_length=20)
+    location = models.ForeignKey(Location)
+
+    
