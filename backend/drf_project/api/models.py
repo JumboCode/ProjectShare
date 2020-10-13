afb6cc389105993f0e34ffb2a0f054c62443fa06
anavianm <anavian.max@gmail.com>
2020-10-13 11:47:39 -0400

    removed uneccessary comments

--- backend/drf_project/api/models.py
+++ backend/drf_project/api/models.py
@@ -5,19 +5,18 @@ from django.db import models
 
 # Create your models here.
 
-
 class Posts(models.Model):
     title = models.CharField(max_length=5000)
     date = models.DateTimeField(auto_now_add = True)
-
-    # which category is better 
-    # category = models.ManyToManyField(Category)
     category = models.ForeignKey(Category) 
-    #tag = models.ManyToManyField(Tag)
     tag = models.ForeignKey(Tag)
     content = models.TextField()
     img = models.ImageField(upload_to = user_directory_path) 
     language = models.CharField(max_length=20)
     location = models.ForeignKey(Location)
 
+class Location(models.Model):
+    latitude = models.CharField(max_length=50)
+    longitude = models.CharField(max_length=50)
 
+    
