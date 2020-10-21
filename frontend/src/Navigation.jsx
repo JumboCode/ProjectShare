777eea0860448b0a0c021870dedb702541ec8000
Meghan Kloud <>
2020-10-21 11:08:05 -0400

    #29 linked navigation component with App.js and printed arbitrary message

---
+++ frontend/src/Navigation.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+// import ReactDOM from 'react-dom';
+import './Navigation.css';
+
+class Navigation extends React.Component {
+  render() {
+  return (
+    <hi>Navigation Bar</hi> 
+   );
+  }
+}
+
+export default Navigation;
