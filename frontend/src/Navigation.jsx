e3a9ef63b9b74d2a7e24fbcfa0a8883de07fe0cc
Meghan Kloud <>
2020-10-21 12:47:36 -0400

    #29 made all of the components of the navigation bar, still need to format using CSS and resolve linters

--- frontend/src/Navigation.jsx
+++ frontend/src/Navigation.jsx
@@ -1,11 +1,39 @@
 import React from 'react';
-// import ReactDOM from 'react-dom';
 import './Navigation.css';
 
-class Navigation extends React.PureComponent {
+class Navigation extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {value: ''};
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({value: event.target.value});
+  }
+
+  handleSubmit(event) {
+    // eslint-disable-next-line
+    const search = this.state.value;
+    // alert(`submited: ${  search}`);
+    event.preventDefault();
+  }
+
   render() {
+    // eslint-disable-next-line react/destructuring-assignment
+    const { value } = this.state.value;
     return (
-      <hi>Navigation Bar</hi>
+      <form onSubmit={this.handleSubmit}>
+        {/* eslint-disable-next-line */}
+        <img src="https://media-exp1.licdn.com/dms/image/C4D0BAQGR7P1s2dm04A/company-logo_200_200/0?e=2159024400&v=beta&t=ExcBhk84mPfea8XkxTN582SzYzOIB0FshET-FS7CMmQ" />
+        <p>Project SHARE</p>
+        <input placeholder="Search" type="text" value={value} onChange={this.handleChange} />
+        <button type="button">
+          Help Me Find a Resource
+        </button>
+      </form>
     );
   }
 }
