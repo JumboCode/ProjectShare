6fc83b0a28d69d8f9c419e4ffacdadda7fcb72f6
Emma Paterson <emmapaterson@resnet158-18.medford.tufts.edu>
2020-11-01 15:49:51 -0500

    Added Button to About Page

--- frontend/src/AboutPage.jsx
+++ frontend/src/AboutPage.jsx
@@ -1,11 +1,18 @@
 import React, { Component } from 'react';
 import './AboutPage.css';
+import Button from 'react-bootstrap/Button';
+
 
 class AboutPage extends Component {
   constructor(props) {
     super(props);
     this.state = {};
   }
+  
+  handleClick = () => {
+    console.log('this is:', this); //placeholder for where button will eventually go
+  }
+
 
   render() {
 
@@ -17,22 +24,22 @@ class AboutPage extends Component {
         <ul className="Spread-Health-Access-Resources-and-Education">
           Spread Health Access, Resources, and Eudcation
         </ul>
-        <ul className="Project-SHARE-focuses-on-reducing-disparities-amo">
+        <ul className="Project-SHARE-focuses-on-reducing-disparities-amon">
           Project SHARE focuses on reducing disparities among Tufts and local communities by advancing access to health care, 
           and providing resources and education in support of the universal right to health.
         </ul>
-        <ul className=".Rectangle-6 "> 
-          <li className="Rectangle-5">
-          </li>
-          <li className="Were-a-team-of-students-at-Tufts-University">
-            We’re a team of students at Tufts University
-          </li>
-          <li className=".Lorem-ipsum-dolor-sit-amet-consectetur-adipiscing">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod 
-            tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, 
-            quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
-            Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
-          </li>
+        <ul className="Rectangle-6"> 
+        </ul>
+        <ul className="Rectangle-5">
+        </ul>
+        <ul className="Were-a-team-of-students-at-Tufts-University">
+          We’re a team of students at Tufts University
+        </ul>
+        <ul className="Lorem-ipsum-dolor-sit-amet-consectetur-adipiscing">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod 
+          tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, 
+          quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
+          Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
         </ul>
         <ul className="Get-involved">
           Get involved
@@ -40,24 +47,24 @@ class AboutPage extends Component {
         <ul className="Lorem-ipsum-dolor-sit-amet-consectetur-adipiscing2">
           Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
         </ul>
-        <ul className="buttonprimary">
-          <li className="View-Club-Website">
-            View Club Website
-          </li>
+        <button className="buttonprimary" type="button" onClick={this.handleClick} > 
+        </button>
+        <ul className="View-Club-Website">
+          View Club Website
         </ul>
-        <ul className="Rectangle-7 ">
-          <li className="Brought-to-you-by-Project-SHARE">
-            Brought to you by Project SHARE
-          </li>
-          <li className="Spreading-Health-Access-Resources-and-Education">
-            Spreading Health Access, Resources, and Education
-          </li>
-          <li className="About">
-            About
-          </li>
-          <li className="Contact-Us">
-            Contact Us
-          </li>
+        <ul className="Rectangle-7"> 
+        </ul>
+        <ul className="Brought-to-you-by-Project-SHARE">
+          Brought to you by Project SHARE
+        </ul>
+        <ul className="Spreading-Health-Access-Resources-and-Education">
+          Spreading Health Access, Resources, and Education
+        </ul>
+        <ul className="About">
+          About
+        </ul>
+        <ul className="Contact-Us">
+          Contact Us
         </ul>
       </div>
     );
