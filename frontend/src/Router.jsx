b83d46c8791ce39e9a673d97a5746649a3adc96e
Linh Tran <linh.tran@tufts.edu>
2020-11-01 14:41:29 -0500

    #30: Removed links from AppRouter and setup routers with Switch

--- frontend/src/Router.jsx
+++ frontend/src/Router.jsx
@@ -2,42 +2,20 @@ import React from "react";
 import {
   BrowserRouter as Router,
   Switch,
-  Route,
-  Link
+  Route
 } from "react-router-dom";
 
 function AppRouter() {
   return ( 
     <Router>
       <div>
-        <ul>
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/about">About</Link>
-          </li>
-        </ul>
-
         <Switch>
-          <Route exact path="/">
-            <Home />
-          </Route>
-          <Route path="/about">
-            <About />
-          </Route>
+          <Route exact path="/" />
+          <Route path="/about" />
         </Switch>
       </div>
     </Router>
   );
-}
-
-function Home() {
-  return <h2>Home</h2>;
-}
-
-function About() {
-  return <h2>About</h2>;
 }
 
 export default React.memo(AppRouter)
