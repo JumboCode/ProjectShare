080cae4938ba97361b229f3ce35d19fa875b2125
Emma Paterson <emmapaterson@resnet158-18.medford.tufts.edu>
2020-11-08 15:41:23 -0500

    created the footer element

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,29 +2,13 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Navigation from './Navigation';
+import FooterElement from './FooterElement';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <Navigation> </Navigation>
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit
-          {' '}
-          <code>src/App.js</code>
-          {' '}
-          and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navigation> </Navigation>
+      <FooterElement> </FooterElement>
     </div>
   );
 }
