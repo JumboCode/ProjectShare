25a6b68153fe66913741bd14c088421809c4ed0f
Anthony Tranduc <anthonytranduc@gmail.com>
2020-11-08 15:51:10 -0500

    #5 merge master

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,31 +1,13 @@
 import React from 'react';
-import AboutPage from './AboutPage'; 
+import AboutPage from './AboutPage';
 import './App.css';
 import Navigation from './Navigation';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <Navigation> </Navigation>
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit
-          {' '}
-          <code>src/App.js</code>
-          {' '}
-          and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App">    
       <AboutPage />
+      <Navigation> </Navigation>
     </div>
   );
 }
