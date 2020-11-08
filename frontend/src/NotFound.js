23d11bfabba43cf05125afc702f05032ba6a96f3
martinma425 <martinma425@gmail.com>
2020-11-08 16:15:17 -0500

    Added a message for the NotFound page

--- frontend/src/NotFound.js
+++ frontend/src/NotFound.js
@@ -4,7 +4,8 @@ import { Link } from "react-router-dom";
 /* NotFound Component: displays the Not Found page */
 const NotFound = () => (
     <div>
-        <h1>404 - Page Not Found!</h1>
+        <h1>404</h1>
+        <p>Sorry, the page you were looking for could not be found.</p>
         <Link to="/">
             Back to Home
         </Link>
