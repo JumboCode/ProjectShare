6d816af29713ae7985b3c4a893f8f252469b7c7b
Meghan Kloud <>
2020-11-28 15:49:02 -0500

    modified template and set up rendering of post page with data in template, still need to format

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,15 +1,17 @@
 import React from 'react';
-import AboutPage from './AboutPage';
+// import AboutPage from './AboutPage';
 import './App.css';
 import Navigation from './Navigation';
 import FooterElement from './FooterElement';
+import Post from './Post';
 
 function App() {
   return (
     <div className="App">
       <Navigation> </Navigation>
-      <AboutPage />
+      {/* <AboutPage /> */}
       <FooterElement> </FooterElement>
+      <Post></Post>
     </div>
   );
 }
