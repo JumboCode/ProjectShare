91cf68af514c71bb952ff3e70dfa902879c997fe
Kristin Ng <nokng0826@gmail.com>
2020-12-06 18:39:43 -0500

    fixed routing, centered 404, added empty index page

--- frontend/src/NotFound.js
+++ frontend/src/NotFound.js
@@ -1,6 +1,5 @@
 import React from 'react';
-import { Link } from "react-router-dom";
-// import './NotFound.css'; 
+import { Link } from "react-router-dom"; 
 
 /* NotFound Component: displays the Not Found page */
 
