b00e206473cebbd1f44eb07fae4a3dd8762fb641
Peter Horvath <peter.horvath@tufts.edu>
2020-12-06 16:17:19 -0500

    created the TagView class
    set serializer_class and queryset attributes

--- backend/drf_project/api/views.py
+++ backend/drf_project/api/views.py
@@ -3,6 +3,13 @@ from __future__ import unicode_literals
 from django.http import JsonResponse
 from rest_framework.views import APIView
 from rest_framework import viewsets
+from .serializers import TagSerializer
+from .models import Tag
+
+
+class TagView(viewsets.ModelViewSet):
+    serializer_class = TagSerializer
+    queryset = Tag.objects.all()
 
 
 class TestView(APIView):
