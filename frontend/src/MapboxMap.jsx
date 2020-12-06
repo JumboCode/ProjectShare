97b3c936bf10148352247e6df52c3f33dbf91cba
Linh Tran <linh.tran@tufts.edu>
2020-12-06 15:44:18 -0500

    #64: Dynamic map resizing and fix code based on comments

--- frontend/src/MapboxMap.jsx
+++ frontend/src/MapboxMap.jsx
@@ -1,9 +1,8 @@
 /* Map feature that displays markers from a list of coords */
 import * as React from 'react';
-import {Component} from 'react';
-import MapGL, {Marker} from 'react-map-gl';
-import 'mapbox-gl/dist/mapbox-gl.css';
-import pin from './pin.png';
+import { Component } from 'react';
+import MapGL, { Marker, WebMercatorViewport } from 'react-map-gl';
+import Pin from './pin.png';
 
 const MAPBOX_TOKEN = process.env.REACT_APP_MAPBOX_ACCESS_TOKEN;
 const places = [
@@ -18,33 +17,65 @@ class Map extends Component {
     super(props);
     this.state = {
       viewport: {
-        width: "100vw",
-        height: "100vh",
+        width: 650,
+        height: 650,
+      },
+      viewState: {
         latitude: 37.7577,
         longitude: -122.4376,
-        zoom: 5
+        zoom: 8
       }
     };
   }
 
-  render() {
+  componentDidMount() { 
+    /* Calculate pairs of min lnglat and max lnglat */
+    const lat = places.map(location => Number(location.pos.lat));
+    const lng = places.map(location => Number(location.pos.lng));
+    
+    const minCoords = [Math.min.apply(null, lng), Math.min.apply(null, lat)];
+    const maxCoords = [Math.max.apply(null, lng), Math.max.apply(null, lat)];
+    const bounds = [minCoords, maxCoords];  
+
+    /* Create new viewport with new bounds from calculations */
     const { viewport } = this.state;
+    
+    const vp = new WebMercatorViewport(viewport); 
+    const {longitude, latitude, zoom} = vp.fitBounds(bounds, {padding: 10});    
+
+    this.setState({ 
+      viewport: {
+        width: "100vw",
+        height: "100vh",
+      }, 
+      viewState: {
+        latitude,
+        longitude,
+        zoom
+      }
+    });
+  }
+
+  render() {
+    const { viewport, viewState } = this.state;
+    
     return (
       <MapGL
         width={viewport.width}
         height={viewport.height}
-        latitude={viewport.latitude}
-        longitude={viewport.longitude}
-        zoom={viewport.zoom}
+        viewState={viewState}
         mapStyle="mapbox://styles/mapbox/streets-v11"
-        // eslint-disable-next-line no-shadow
-        onViewportChange={viewport => this.setState({viewport})}
+        onViewportChange={vs => this.setState({viewState: vs})}
         mapboxApiAccessToken={MAPBOX_TOKEN}
       >
         {
           places.map(
             city => 
-              <Marker key={city.id} longitude={city.pos.lng} latitude={city.pos.lat}><img alt="pin" src={pin} /></Marker>
+              (
+                <Marker key={city.id} longitude={city.pos.lng} latitude={city.pos.lat}>
+                  <img src={Pin} alt="pin" width="32px" height="32px" />
+                </Marker>
+              )
           )
         }
       </MapGL>
@@ -52,4 +83,4 @@ class Map extends Component {
   }
 }
 
-export default Map;
+export default Map; 
