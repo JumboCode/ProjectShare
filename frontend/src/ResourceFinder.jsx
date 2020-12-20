1a7c437db2d784105a3375f1e479f46334916af7
Meghan Kloud <>
2020-12-20 12:28:13 -0500

    conditional selection for help me find a resource functionality finished

--- frontend/src/ResourceFinder.jsx
+++ frontend/src/ResourceFinder.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 
-// still needs to be fixed.
 class ResourceFinder extends React.Component {
   constructor(props) {
     super(props);
@@ -8,7 +7,6 @@ class ResourceFinder extends React.Component {
     this.handleChangeRes = this.handleChangeRes.bind(this);
     this.state = {
       locationSelected: false,
-      resourceSelected: false,
       location: "",
       resource: "",
     };
@@ -23,51 +21,44 @@ class ResourceFinder extends React.Component {
 
   handleChangeRes(props) {
     this.setState({
-      resourceSelected: true,
       resource: props.target.value,
     })
   }
 
   render() {
     const { locationSelected } = this.state;
-    const { resourceSelected } = this.state;
+    // eslint-disable-next-line no-unused-vars
     const { location } = this.state;
+    // eslint-disable-next-line no-unused-vars
     const { resource } = this.state;
+    const selectLoc = (
+      <div className="FirstSelection">
+        <select name="LocationType" location={this.state} onChange={this.handleChangeLoc}>
+          <option value="default" selected disabled hidden>Choose Location</option>
+          <option value="Tufts">Tufts</option>
+          <option value="Sherwood">Sherwood</option>
+          <option value="Medford">Medford</option>
+        </select>
+      </div>
+    );
+    const selectRes = (
+      <div className="SecondSelection">
+        <select name="ResourceType" resource={this.state} onChange={this.handleChangeRes}>
+          <option value="default" selected disabled hidden>Choose Resource</option>
+          <option value="COVID-19">COVID-19</option>
+          <option value="default" selected disabled hidden>Choose Resource</option>
+          <option value="Woman's Health">Woman&apos;s Health</option>
+          <option value="default" selected disabled hidden>Choose Resource</option>
+          <option value="LGBT">LGBT</option>
+        </select>
+      </div>
+    );
     let select;
 
     if (!locationSelected) {
-      select = (
-        <div className="FirstSelection">
-          <select name="LocationType" location={this.state} onChange={this.handleChangeLoc}>
-            <option value="default" selected disabled hidden>Choose Location</option>
-            <option value="Tufts">Tufts</option>
-            <option value="Sherwood">Sherwood</option>
-            <option value="Medford">Medford</option>
-          </select>
-        </div>
-      );
+      select = selectLoc;
     } else {
-      select = (
-        <div className="SecondSelection">
-          <select name="ResourceType" resource={this.state} onChange={this.handleChangeRes}>
-            <option value="default" selected disabled hidden>Choose Resource</option>
-            <option value="COVID-19">COVID-19</option>
-            <option value="Woman's Health">Woman&apos;s Health</option>
-            <option value="LGBT">LGBT</option>
-          </select>
-        </div>
-      );
-    }
-
-    // This is for testing purposes to make sure that our values are stored correctly
-    if (resourceSelected) {
-      select = 
-      (
-        <p>
-          {location}
-          {resource}
-        </p>
-      );
+      select = selectRes;
     }
 
     return (
