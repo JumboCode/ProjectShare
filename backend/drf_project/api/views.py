68cf98454f653c9303b728ab8a4de8da81afbd34
Meguna Okawa <megunaokawa@gmail.com>
2021-01-11 15:33:22 +0900

    update tags and categories list view to simply return name values

--- backend/drf_project/api/views.py
+++ backend/drf_project/api/views.py
@@ -4,6 +4,7 @@ from rest_framework import viewsets, permissions
 from .serializers import TagSerializer, CategorySerializer, \
     PostSerializer, ImageSerializer
 from .models import Tag, Category, Post, Image
+from rest_framework.response import Response
 
 
 class TagViewSet(viewsets.ModelViewSet):
@@ -12,12 +13,22 @@ class TagViewSet(viewsets.ModelViewSet):
     serializer_class = TagSerializer
     permission_classes = [permissions.IsAuthenticatedOrReadOnly]
 
+    # only return the contents of the name field
+    def list(self, request, *args, **kwargs):
+        queryset = self.filter_queryset(self.get_queryset())
+        return Response(queryset.values_list('name', flat=True))
+
 
 class CategoryViewSet(viewsets.ModelViewSet):
     lookup_url_kwarg = 'category_id'
     queryset = Category.objects.all()
     serializer_class = CategorySerializer
     permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
+    # only return the contents of the name field
+    def list(self, request, *args, **kwargs):
+        queryset = self.filter_queryset(self.get_queryset())
+        return Response(queryset.values_list('name', flat=True))
 
 
 class ImageViewSet(viewsets.ModelViewSet):
