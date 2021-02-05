80d96291099d871a604af2a2bfee841a8b240e6b
Meguna Okawa <megunaokawa@gmail.com>
2021-02-05 14:18:58 -0500

    clean up PR

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,19 +1,19 @@
 import React from 'react';
+import AboutPage from './AboutPage';
 import './App.css';
 import Navigation from './Navigation';
 import FooterElement from './FooterElement';
-import Post from './Post';
+import PostContent from './PostContent';
 
 function App() {
   return (
     <div className="App">
       <Navigation> </Navigation>
+      <AboutPage />
       <FooterElement> </FooterElement>
-      <Post />
+      <PostContent> </PostContent>
     </div>
   );
 }
 
-export default App; 
-
- 
+export default App;
