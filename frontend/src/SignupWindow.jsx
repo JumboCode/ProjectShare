03dfedc9f283d45f36d2e6f2b7a43a6e69577363
Meghan Kloud <>
2021-02-27 17:07:16 -0500

    Now able to retrieve a key. Fixed console errors and indentation errors.

--- frontend/src/SignupWindow.jsx
+++ frontend/src/SignupWindow.jsx
@@ -8,9 +8,9 @@ class Signup extends React.Component {
         super(props);
         this.handleSubmit = this.handleSubmit.bind(this);
         this.state = {
-            userEmail: "",
+            useremail: "",
             password1: "",
-            password2: "",
+            password2: ""
         };
     }
 
@@ -19,57 +19,58 @@ class Signup extends React.Component {
             alert( 'Passwords have to match.' );
         } else {
             // response stores the key fetched from the signup endpoint
+            // eslint-disable-next-line
             const response = fetch('http://localhost:8000/auth/signup/', {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json',
                 },
                 body: JSON.stringify({
-                    email: this.state.userEmail,
+                    email: this.state.useremail,
                     password1: this.state.password1,
                     password2: this.state.password2
                 })
-            });
+            })
         }
     }
 
     render() {
         return (
             <Form className="signupForm" onSubmit={ this.handleSubmit }>
-            <Form.Group className="email" controlID="userEmail">
-                <Form.Label>Email   </Form.Label>
-                <Form.Control 
-                    type="email" 
-                    placeholder="Enter Email" 
-                    userEmail={ this.state }
-                    onChange={ input => this.setState({ userEmail : input.target.value})}
+                <Form.Group className="email" controlid="useremail">
+                    <Form.Label>Email   </Form.Label>
+                    <Form.Control 
+                        type="email" 
+                        placeholder="Enter Email" 
+                        useremail={ this.state }
+                        onChange={ input => this.setState({ useremail : input.target.value})}
                     ></Form.Control>
-            </Form.Group>
+                </Form.Group>
 
-            <Form.Group className="password1" controlID="userPassword1">
-                <Form.Label>Password   </Form.Label>
-                <Form.Control 
-                    type="password" 
-                    placeholder="Password" 
-                    value={ this.state.password1 }
-                    onChange={ input => this.setState({ password1 : input.target.value})}
+                <Form.Group className="password1" controlid="userPassword1">
+                    <Form.Label>Password   </Form.Label>
+                    <Form.Control 
+                        type="password" 
+                        placeholder="Password" 
+                        value={ this.state.password1 }
+                        onChange={ input => this.setState({ password1 : input.target.value})}
                     ></Form.Control>
-            </Form.Group>
+                </Form.Group>
 
-            <Form.Group className="password2" controlID="userPassword2">
-                <Form.Label>Enter Password Again   </Form.Label>
-                <Form.Control
-                type="password" 
-                placeholder="Password" 
-                value={ this.state.password2 }
-                onChange={ input => this.setState({ password2 : input.target.value})}
-                ></Form.Control>
-            </Form.Group>
+                <Form.Group className="password2" controlid="userPassword2">
+                    <Form.Label>Enter Password Again   </Form.Label>
+                    <Form.Control
+                        type="password" 
+                        placeholder="Password" 
+                        value={ this.state.password2 }
+                        onChange={ input => this.setState({ password2 : input.target.value})}
+                    ></Form.Control>
+                </Form.Group>
 
-            <Button className="submit" variant="primary" type="submit">
-                Submit
-            </Button>
-        </Form>
+                <Button className="submit" variant="primary" type="submit">
+                    Submit
+                </Button>
+            </Form>
 
         );
     }
