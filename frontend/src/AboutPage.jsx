1cf975d1cb0f4eef5d460298c050905df0b9a42b
Anthony Tranduc <anthonytranduc@gmail.com>
2021-03-06 23:37:21 -0500

    #127 implemented intro and club bio

--- frontend/src/AboutPage.jsx
+++ frontend/src/AboutPage.jsx
@@ -14,39 +14,34 @@ class AboutPage extends Component {
 
   render() {
     return (
-      <div>
-        <div className="landing">
-          <ul className="lets-SHARE"> 
-            Let&apos;s SHARE
-          </ul>
-          <ul className="Spread-Health-Access-Resources-and-Education">
-            Spread Health Access, Resources, and Eudcation
-          </ul>
-          <ul className="Project-SHARE-focuses-on-reducing-disparities-amon">
-            Project SHARE focuses on reducing disparities among Tufts and local communities by advancing access to health care, 
-            and providing resources and education in support of the universal right to health.
-          </ul>
-          <ul className="Rectangle-6" /> 
-          <ul className="Rectangle-5" />
-          <ul className="Were-a-team-of-students-at-Tufts-University">
-            We’re a team of students at Tufts University
-          </ul>
-          <ul className="Lorem-ipsum-dolor-sit-amet-consectetur-adipiscing">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod 
-            tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, 
-            quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
-            Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
-          </ul>
-          <ul className="Get-involved">
-            Get involved
-          </ul>
-          <ul className="Lorem-ipsum-dolor-sit-amet-consectetur-adipiscing2">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-          </ul>
-          <Button className="buttonprimary" type="button" onClick={this.handleClick} />
-          <ul className="View-Club-Website">
-            View Club Website
-          </ul>
+      <div className="landing">
+        <div className="intro">
+          <h1>Let&apos;s SHARE</h1>
+          <h2>Spread Health Acess, Resources, and Education</h2>
+          <body>
+            Project SHARE focuses on reducing disparities among Tufts and local 
+            communities by advancing access to health care, and providing 
+            resources and education in support of the universal right to health.​
+          </body>
+        </div>
+        <div className="middle-strip">
+          <div className="club-bio">
+            <img className="bio-picture" src="https://breakthrough.org/wp-content/uploads/2018/10/default-placeholder-image.png" alt="Project SHARE club" />
+            <div className="bio-text">
+              <h2>We&apos;re a team of students at Tufts University</h2>
+              <body>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do 
+                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim 
+                ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut 
+                aliquip ex ea commodo consequat. Duis aute irure dolor in 
+                reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla 
+                pariatur.
+              </body>
+              <button className="bio-button" type="button">
+                View Club Website
+              </button>
+            </div>
+          </div>
         </div>
       </div>
     );
