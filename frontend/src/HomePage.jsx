7f8f58bc275da12b74168b850071407dd940bdbe
Kristin <nokng0826@gmail.com>
2021-03-13 16:48:49 -0500

    added div class for sidebar, fixed resource button

--- frontend/src/HomePage.jsx
+++ frontend/src/HomePage.jsx
@@ -19,27 +19,29 @@ function Home(){
   );
   return (
     <div className="HomePage">
-      <div className="sortBy"> 
-        <h3 className="sortByHeader"> Sort By </h3>
-        <DropdownButton variant="outline-primary" className="mostRecent" title="Most Recent"> 
-          <Dropdown.Item> Something </Dropdown.Item>
-          <DropdownItem> Something 2</DropdownItem>
-        </DropdownButton>
+      <div className="sideBar">
+        <div className="sortBy"> 
+          <h3 className="sortByHeader"> Sort By </h3>
+          <DropdownButton variant="outline-primary" className="mostRecent" title="Most Recent"> 
+            <Dropdown.Item> Something </Dropdown.Item>
+            <DropdownItem> Something 2</DropdownItem>
+          </DropdownButton>
+        </div>
+        <div className="topicList"> 
+          <h3 className="filters">
+            Filters
+          </h3>
+          <ul className="filterList">
+            {listItems} 
+          </ul>
+        </div>
+        <div className="needHelp">  
+          <h3 className="helpHeader"> Need Help? </h3>
+          <p className="helpParagraph"> Use our resource finder to find the information you are looking for. </p>
+          <Button variant="primary" className="helpButton"> Resource Finder </Button>
+        </div>
       </div>
-      <div className="topicList"> 
-        <h3 className="filters">
-          Filters
-        </h3>
-        <ul className="filterList">
-          {listItems} 
-        </ul>
-      </div>
-      <div className="needHelp">  
-        <h3 className="helpHeader"> Need Help? </h3>
-        <p className="helpParagraph"> Use our resource finder to find the information you are looking for. </p>
-        <Button variant="primary" className="helpButton"> Resource Finder </Button>
-      </div>
-      <div className="featured">  </div>
+      <div className="featured">  </div> 
     </div>
   );
 }
