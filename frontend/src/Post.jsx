0498e290ae03e1c522f2c4d85e51599dd9fb65dd
Emma Paterson <emmapaterson@resnet158-18.medford.tufts.edu>
2021-03-21 16:17:01 -0400

    139 Integrated Post with Backend

--- frontend/src/Post.jsx
+++ frontend/src/Post.jsx
@@ -15,41 +15,70 @@ const templateData = [{
 class Post extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {posts:[]};
   }
 
-  render() {;
-    const post = templateData[0]
+  componentDidMount() {
+    const { match: { params } } = this.props;
+    fetch ("http://localhost:8000/api/posts") 
+      .then(res => res.json())
+      .then(res => this.setState({posts: res}));
+  }
+
+  render() {
+    const {posts} = this.state
+    let post;
+    if (posts.length !== 0) {
+      post = posts[posts.length - 1]
+    }
     return (
       <div className="post">
-        <p className="tags">
-          {post.tags.map(tag => (
-            <a href="/" className="tagElem" key={tag}>{tag}</a>
-          ))}
-        </p>
-        <p className="title">
-          { post.title }
-        </p>
-        <p className="author">
-          By Project SHARE
-        </p>
-        <p className="date">
-          { post.date }
-        </p>
-        <p>
-          <div className="image">
-            {/* Image */}
+        { posts.length > 0 && (
+          <p className="tags">
+            {post.tags.map(tag => (
+              <a href="/" className="tagElem" key={tag.name}>{tag.name}</a>
+            ))}
+          </p>
+        )}
+        { posts.length > 0 && (
+          <p className="title">
+            { post.title } 
+          </p> 
+        )}
+        { posts.length > 0 && (
+          <p className="author">
+            By Project SHARE
+          </p>
+        )}
+        { posts.length > 0 && (
+          <p className="date">
+            { post.date }
+          </p>
+        )}
+        { posts.length > 0 && (
+          <p>
+            <div className="image"> 
+              {post.images.map(image => (
+                <img src={image.img_file} alt={image.img_name} key={image.id} className="image" />
+              ))}
+            </div>
+          </p>
+        )}
+        { posts.length > 0 && (
+          <p className="caption">
+            { post.caption }
+          </p>
+        )}
+        { posts.length > 0 && (
+          <p className="contents">
+            { post.content }
+          </p>
+        )}
+        { posts.length > 0 && (
+          <div className="map">
+            <Map />
           </div>
-        </p>
-        <p className="caption">
-          { post.caption }
-        </p>
-        <p className="contents">
-          { post.content }
-        </p>
-        <div className="map">
-          <Map />
-        </div>
+        )}
       </div>
     );
   }
