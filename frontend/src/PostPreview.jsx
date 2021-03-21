31ae675b4219288bffa661b633033c011c0e390d
Anthony Tranduc <anthonytranduc@gmail.com>
2021-03-21 16:41:56 -0400

    #106 eslint

--- frontend/src/PostPreview.jsx
+++ frontend/src/PostPreview.jsx
@@ -3,32 +3,32 @@ import './PostPreview.css';
 
 
 function PostPreview({ Data }) {
-    return (
+  return (
     <div className="PostPreview">
-        <p>
-            <div className="PhotoPlaceHolder">
-                {/* Image */}
-            </div>
-        </p>
-        <h5 className="DatePosted">
-            { Data.date }
-        </h5>
-        <div className="Title">
-            { Data.title }
+      <p>
+        <div className="PhotoPlaceHolder">
+          {/* Image */}
         </div>
-        <div className="PostContent">
-            {
-                `${Data.content.substr(0, 113)}...`
-            }
-        </div>
-        <div text="..."> </div>
-        <p className="Tags">
-          {Data.tags.map(tag => (
-            <a href="/" className="tagElemPost" key={tag}>{tag}</a>
-          ))}
-        </p>
+      </p>
+      <h5 className="DatePosted">
+        { Data.date }
+      </h5>
+      <div className="Title">
+        { Data.title }
+      </div>
+      <div className="PostContent">
+        {
+          `${Data.content.substr(0, 113)}...`
+        }
+      </div>
+      <div text="..."> </div>
+      <p className="Tags">
+        {Data.tags.map(tag => (
+          <a href="/" className="tagElemPost" key={tag}>{tag}</a>
+        ))}
+      </p>
     </div>
-    )
+  )
 }
 
 export default PostPreview;
