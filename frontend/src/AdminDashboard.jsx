8817b4c7f489de55e49dcb7d1cbbb9f90aaed403
Meghan Kloud <>
2021-03-21 15:49:43 -0400

    fixed indentation and tags

--- frontend/src/AdminDashboard.jsx
+++ frontend/src/AdminDashboard.jsx
@@ -3,57 +3,59 @@ import { Table } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 class Dashboard extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            isLoading: true,
-            fetchData: [],
-        };
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      isLoading: true,
+      fetchData: [],
+    };
+  }
 
-    componentDidMount() {
-        fetch('http://localhost:8000/api/posts')
-            .then(response => response.json())
-            .then(data => 
-                this.setState({
-                    fetchData: data,
-                    isLoading: false,
-                })
-            )
-            .catch(error => console.error('Error: Could not fetch data.'));
-    }
+  componentDidMount() {
+    fetch('http://localhost:8000/api/posts')
+      .then(response => response.json())
+      .then(data => 
+        this.setState({
+          fetchData: data,
+          isLoading: false,
+        })
+      )
+      .catch(error => console.error('Error: Could not fetch data.'));
+  }
 
-    render() {
-        const { isLoading, fetchData } = this.state
-        if(!isLoading) {
-            return (
-                <Table striped bordered className="DataTable">
-                    <thead>
-                        <tr>
-                            <th>Title</th>
-                            <th>Date</th>
-                            <th>Tags</th>
-                            <th>Category</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {fetchData.map((post,index) =>
-                            <tr key={index}>
-                                <td>{post.title}</td>
-                                <td>{post.date}</td>
-                                <td>{post.tags}</td>
-                                <td>{post.category.name}</td>
-                            </tr> 
-                        )}
-                    </tbody>
-                </Table>
-            );
-        } else {
-            return (
-                <h3>Loading...</h3>
-            );
-        }
+  render() {
+    const { isLoading, fetchData } = this.state
+    if(!isLoading) {
+      return (
+        <Table striped bordered className="DataTable">
+          <thead>
+            <tr>
+              <th>Title</th>
+              <th>Date</th>
+              <th>Tags</th>
+              <th>Category</th>
+            </tr>
+          </thead>
+          <tbody>
+            {fetchData.map((post,index) =>
+              <tr key={index}>
+                <td>{post.title}</td>
+                <td>{post.date}</td>
+                <td>{post.tags.map((tags, i) => 
+                  <p key={i}>{tags.name}</p> )}
+                </td>
+                <td>{post.category.name}</td>
+              </tr> 
+            )}
+          </tbody>
+        </Table>
+      );
+    } else {
+      return (
+        <h3>Loading...</h3>
+      );
     }
+  }
 }
 
 export default Dashboard;
