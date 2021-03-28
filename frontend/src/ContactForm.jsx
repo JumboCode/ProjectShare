a1be0265fce4dfc12bb7b739318bfc1cd5b106d8
Anthony Tranduc <anthonytranduc@gmail.com>
2021-03-28 16:44:51 -0400

    #113 Adding custom popup alerts with new component. Form clears after submit

--- frontend/src/ContactForm.jsx
+++ frontend/src/ContactForm.jsx
@@ -1,15 +1,18 @@
 import React from 'react';
+import PopUpAlert from './PopUpAlert';
 import './ContactForm.css';
 
 class ContractForm extends React.Component {
   constructor(props) {
     super(props);
     this.state = {email: '',
-      message: '',};
+      message: '',
+      alert: false,
+      status: true};
     
     this.onChange = this.onChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
-    this.resetForm = this.resetForm.bind(this);
+    this.onShowAlert = this.onShowAlert.bind(this);
   }
 
   onChange(event) {
@@ -19,13 +22,16 @@ class ContractForm extends React.Component {
     this.setState({[name]: value});
   }
 
-  resetForm() {
-    this.setState({email: '', message: ''})
+  onShowAlert() {
+    this.setState({alert: true});
+    window.setTimeout(()=>{
+      this.setState({alert: false})
+    }, 3000)
   }
 
   handleSubmit(event) {
     event.preventDefault();
-    fetch('/api/contact', {
+    fetch('http://localhost:8000/api/contact', {
       method: "POST",
       body: JSON.stringify(this.state),
       credentials: "same-origin",
@@ -37,10 +43,13 @@ class ContractForm extends React.Component {
       (response) => (response.json())
     ).then((response) => {
       if (response.status === 'success') {
-        alert("Message sent!");
-        this.resetForm();
+        this.setState({status: true,
+          email:'',
+          message: ''});
+        this.onShowAlert();
       } else if (response.data.status === 'fail') {
-        alert("Message failed to send.");
+        this.setState({status: false});
+        this.onShowAlert();
       }
     })
   }
@@ -48,29 +57,37 @@ class ContractForm extends React.Component {
 
 
   render() {
+    const {email, message, alert, status} = this.state;
 
     return (
       <div className="contact-form-container">
+        <PopUpAlert 
+          variant={status ? 'success' : 'danger'}
+          visible={alert}
+          message={status ? 'Your message was sent!' : 'Error in sending message. Please try again.'}
+        />
         <h2 className="title-text">Send us a message!</h2>
         <body className="additional-text">
           Lorem ipsum dolor sit amet, consectetur adipiscing elit.
         </body>
-        <form className="contact-form" onSubmit={this.handleSubmit}>
+        <form className="contact-form">
           <input 
             name="email"
             type="email" 
+            value={email}
             className="email-field" 
             placeholder="Email Address"
             onChange={this.onChange}
           />
           <textarea 
             name="message" 
+            value={message}
             className="message-field"
             placeholder="Message" 
             rows="7" 
             onChange={this.onChange}
           />
-          <button className="submit-button" type="submit">Submit</button>
+          <button onClick={this.handleSubmit} className="submit-button" type="submit">Submit</button>
         </form>
       </div>
     );
