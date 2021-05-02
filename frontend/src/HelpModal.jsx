ea249aa93d68957d8cf6df81a4cadc252c240d69
Meguna Okawa <megunaokawa@gmail.com>
2021-05-02 18:19:54 -0400

    run linter and remove Modal.Dialog

--- frontend/src/HelpModal.jsx
+++ frontend/src/HelpModal.jsx
@@ -12,7 +12,7 @@ class HelpModal extends React.Component {
   }
 
   componentDidMount() {
-      fetch('http://localhost:8000/api/tags')
+    fetch('http://localhost:8000/api/tags')
       .then(res => res.json())
       // .then(res => console.log(res))
       .then(res => this.setState({tags: res}))
@@ -20,58 +20,63 @@ class HelpModal extends React.Component {
 
   onTagClick(event) {
     const {target: selectedTag} = event;
-    if (selectedTag.id in this.state.tags) {
-      let tagsModified = this.state.tags.filter(tag => tag.id !== selectedTag.id)
+    const { tags } = this.state;
+    if (selectedTag.id in tags) {
+      const tagsModified = tags.filter(tag => tag.id !== selectedTag.id)
       this.setState({ selectedTag: tagsModified })
     } else {
       this.setState({
-        selectedTag: [...this.state.tags, selectedTag]
+        selectedTag: [...tags, selectedTag]
       })
     }
 
   }
 
   modalPage = () => {
-      this.setState({modalPage: "locations"})
+    this.setState({modalPage: "locations"})
   }
 
   closeModal = () => {
-      this.setState({isModalOpen: false})
+    this.setState({isModalOpen: false})
   }
 
   render() {
     const {tags, isModalOpen, modalPage} = this.state
     return (
       <div>
-            <Modal show={isModalOpen}>
-                  <Modal.Dialog>
-                    <Modal.Header closeButton>
-                        <Modal.Title variant="header">Resource Finder</Modal.Title>
-                    </Modal.Header>
-                        {modalPage === "tags" && (
-                            <div>
-                                <Modal.Body>Select topics you are interested in: </Modal.Body>
-                                <Modal.Body>
-                                    {tags && tags.map(tag=> (
-                                        <div key = {tag.id}>
-                                            <input type="checkbox" className="checkbox" variant="primary" onClick={this.onTagClick}/>
-                                            <label for="tag.id" className="tagid" variant="paragraph"> {tag.name} </label>
-                                        </div>
-                                    ))}
-                                </Modal.Body>
-                            </div>
-                        )}
-                    <Modal.Footer>
-                        <Button variant="outline-primary" onClick={this.closeModal}>Cancel</Button>{' '}
-                        {modalPage === "tags" && (
-                            <div>
-                                <Button variant="primary" onClick={this.modalPage}>Next</Button>{' '}
-                            </div>
-                        )}
-                    </Modal.Footer>
-                </Modal.Dialog>
-            </Modal>
-    </div>
+        <Modal show={isModalOpen} size="lg">
+          <Modal.Header closeButton>
+            <Modal.Title variant="header">Resource Finder</Modal.Title>
+          </Modal.Header>
+          {modalPage === "tags" && (
+            <div>
+              <Modal.Body>
+                <p className="modal-p-text"> Select topics you are interested in:</p>
+                {tags && tags.map(tag=> (
+                  <div key={tag.id}>
+                    <input type="checkbox" className="checkbox" variant="primary" onClick={this.onTagClick} />
+                    <label htmlFor="tag.id" className="tagid" variant="paragraph"> 
+                      {' '}
+                      {tag.name}
+                      {' '}
+                    </label>
+                  </div>
+                ))}
+              </Modal.Body>
+            </div>
+          )}
+          <Modal.Footer>
+            <Button variant="outline-primary" onClick={this.closeModal}>Cancel</Button>
+            {' '}
+            {modalPage === "tags" && (
+              <div>
+                <Button variant="primary" onClick={this.modalPage}>Next</Button>
+                {' '}
+              </div>
+            )}
+          </Modal.Footer>
+        </Modal>
+      </div>
     );
   }
 }
