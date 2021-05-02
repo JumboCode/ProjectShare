5fc4e1a525e0f4e34698c980cd64368a635ac304
Meguna Okawa <megunaokawa@gmail.com>
2021-05-02 13:15:54 -0400

    update proptypes on footer component

--- frontend/src/FooterElement.jsx
+++ frontend/src/FooterElement.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { PropTypes } from 'prop-types';
 import { Link } from "react-router-dom";
 import './FooterElement.css';
 
@@ -31,3 +32,7 @@ class FooterElement extends Component {
   }
 }
 export default FooterElement;
+
+FooterElement.propTypes = {
+  isAuthenticated: PropTypes.bool.isRequired,
+}
