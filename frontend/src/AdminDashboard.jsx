75e1a3397adaa1d87a4f6b961cfd5e52f7f2b2b6
Meguna Okawa <megunaokawa@gmail.com>
2021-05-27 15:47:35 +0900

    add post composer to admin dashboard and fix styles

--- frontend/src/AdminDashboard.jsx
+++ frontend/src/AdminDashboard.jsx
@@ -1,6 +1,7 @@
 import React from "react";
-import { Table } from 'react-bootstrap';
+import { Table , Button } from 'react-bootstrap';
 import './AdminDashboard.css';
+import { Link } from "react-router-dom";
 import UpdateFeaturedPost from './UpdateFeaturedPost';
 import { BACKEND_URL } from './fetch';
 
@@ -28,9 +29,15 @@ class Dashboard extends React.Component {
   render() {
     const { isLoading, fetchData } = this.state;
     const { authToken } = this.props;
+    const dateOptions = { year: 'numeric', month: 'long', day: 'numeric', hour: '2-digit', minute: '2-digit' };
     if(!isLoading) {
       return (
         <div className="admin-page-container">
+          <h2>Add A New Resource</h2>
+          <Button className="add-post-button" variant="primary" type="button">
+            <Link to="add-post">Add A Resource</Link>
+          </Button>
+          <h2>Manage All Resources</h2>
           <Table striped bordered className="DataTable">
             <thead>
               <tr>
@@ -44,7 +51,7 @@ class Dashboard extends React.Component {
               {fetchData.map((post,index) => (
                 <tr key={index}>
                   <td>{post.title}</td>
-                  <td>{post.date}</td>
+                  <td>{new Date(post.date).toLocaleDateString("en-US", dateOptions)}</td>
                   <td>
                     {post.tags.map((tags, i) => 
                       <p key={i}>{tags.name}</p> )}
@@ -55,6 +62,7 @@ class Dashboard extends React.Component {
               )}
             </tbody>
           </Table>
+          <h2>Manage Featured Resources</h2>
           <UpdateFeaturedPost authToken={authToken} />
         </div>
       );
